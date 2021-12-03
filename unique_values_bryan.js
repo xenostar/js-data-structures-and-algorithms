c136b9c7e6dd9cee48149ce2c000307fac0f6e7a
xenostar <bryan.a.leewood@gmail.com>
2021-12-03 10:35:00 -0800

    main Adding new algos.

---
+++ unique_values_bryan.js
@@ -0,0 +1,6 @@
+const countUniqueValues = arr => [...new Set(arr)].length;
+
+countUniqueValues([1, 1, 1, 1, 1, 2]); //?
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); //?
+countUniqueValues([]); //?
+countUniqueValues([-2, -1, -1, 0, 1]); //?
